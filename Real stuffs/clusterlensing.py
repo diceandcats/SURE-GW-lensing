ccedc2483cfa05660a1e4cd2eb83855dbef0322f
diceandcats <briancheng88@gmail.com>
2024-07-15 18:17:07 +0100

    15/7

--- Real stuffs/clusterlensing.py
+++ Real stuffs/clusterlensing.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 import scipy.optimize._minimize as minimize
-
+from astropy.cosmology import FlatLambdaCDM
+import lenstronomy.Util.constants as const
 
 class ClusterLensing:
     """
@@ -29,22 +30,22 @@ class ClusterLensing:
         self.lens_potential_map = lens_potential_map
         self.z_l = z_l
         self.z_s = z_s
-        self.pixelscale = pixscale
+        self.pixscale = pixscale
         self.size = size
-        self.x_src = x_src
-        self.y_src = y_src
+        self.x_src = x_src / pixscale   #in pixel now
+        self.y_src = y_src / pixscale   #in pixel now
         self.image_positions = None
         self.magnifications = None
         self.time_delays = None
 
 
-    def find_rough_def_pix(self):    # data are in pixel
+    def find_rough_def_pix(self):    # result are in pixel
         """
         Find the pixels that can ray-trace back to the source position roughly.
         """
         alpha_x = self.alpha_map_x   # make sure alpha_x and alpha_y are in pixel
         alpha_y = self.alpha_map_y
-        coord = (self.x_src, self.y_src)
+        coord = (self.x_src, self.y_src)  # in pixel
         coord_x_r, coord_y_r = coord[0] % 1, coord[1] % 1
         y_round, x_round = round(coord[1]), round(coord[0])
 
@@ -55,6 +56,7 @@ class ClusterLensing:
         coordinates = []
         n = 0
         size = self.size
+
         # Iterate over a pre-defined range, assuming alpha_y_2d and alpha_x_2d are indexed appropriately
         for i in range(size):
             for j in range(size):
@@ -62,18 +64,18 @@ class ClusterLensing:
                 if round(ycoord) in y_possible_rounds and round(xcoord) in x_possible_rounds:
                     coordinates.append((j, i))  # (x, y)
                     n += 1
-        #pixscale = self.pixelscale
+        #pixscale = self.pixscale
         #plt.scatter([i[0]*pixscale for i in coordinates], [i[1]*pixscale for i in coordinates], c='r', s=1)
         #plt.scatter(coord[0]*pixscale, coord[1]*pixscale, c='b', s=1)
     
-        return coordinates
+        return coordinates   # in pixel
 
     def def_angle_interpolate(self, x,y, alpha_x= None, alpha_y = None):  #(x,y) is img_guess
         """
         Interpolate the deflection angle at the image position.
         """
-        alpha_x = np.array(self.alpha_map_x, dtype=np.float64)
-        alpha_y = np.array(self.alpha_map_y, dtype=np.float64)
+        alpha_x = np.array(self.alpha_map_x, dtype=np.float64)    #in pixel
+        alpha_y = np.array(self.alpha_map_y, dtype=np.float64)    #in pixel
     
         dx = x - floor(x)
         dy = y - floor(y)
@@ -85,15 +87,15 @@ class ClusterLensing:
         bottom = bottom_left * (1 - dx) + bottom_right * dx
         alpha = top * dy + bottom *(1 - dy)
         src_guess = np.array([x-alpha[0], y-alpha[1]])
-        return src_guess, alpha
+        return src_guess, alpha     # in pixel
     
 
     def diff_interpolate (self, img_guess):
         """
         Difference between the guessed source position and the real source position.
         """
-        real_src = (self.x_src, self.y_src)
-        src_guess = self.def_angle_interpolate(img_guess[0],img_guess[1])[0]
+        real_src = (self.x_src, self.y_src)   # in pixel
+        src_guess = self.def_angle_interpolate(img_guess[0],img_guess[1])[0]    # in pixel
         return np.sqrt((src_guess[0]-real_src[0])**2 + (src_guess[1]-real_src[1])**2)
     
     def get_image_positions(self, pixscale = None):
@@ -110,7 +112,7 @@ class ClusterLensing:
         ---------------
         image_positions: The image positions of the source in arcsec.
         """
-        pixscale = self.pixelscale
+        pixscale = self.pixscale
         #Separate the images
         coordinates = np.array(self.find_rough_def_pix()) #data in pixel
 
@@ -133,11 +135,11 @@ class ClusterLensing:
 
         #for i in range(len(images)):
             #plt.scatter(images[i][:,0], images[i][:,1], s=0.5)
-        print(f'Number of pixels: {[np.sum(len(images[i])) for i in range(len(images))]}')
+        #print(f'Number of pixels: {[np.sum(len(images[i])) for i in range(len(images))]}')
         
         # Get the image positions
-        plt.scatter(self.x_src* pixscale, self.y_src* pixscale, c='b')
-        plt.scatter([i[0]* pixscale for i in coordinates], [i[1]* pixscale for i in coordinates], c='y', s=5)
+        plt.scatter(self.x_src* pixscale, self.y_src* pixscale, c='b')                                            #plot in arcsec
+        plt.scatter([i[0]* pixscale for i in coordinates], [i[1]* pixscale for i in coordinates], c='y', s=5)     #plot in arcsec
 
         img = [[] for _ in range(len(images))]
        
@@ -147,8 +149,116 @@ class ClusterLensing:
             y_max, y_min = np.max(images[i][:,1]), np.min(images[i][:,1])
             img_guess = (np.random.uniform(x_min, x_max), np.random.uniform(y_min, y_max))
             pos = minimize.minimize(self.diff_interpolate, img_guess, bounds =[(x_min-2, x_max+2), (y_min-2, y_max+2)], method='L-BFGS-B', tol=1e-7) # the 2 is for wider boundary
-            print(x_min* pixscale, x_max* pixscale, y_min* pixscale, y_max* pixscale, pos.x* pixscale, self.diff_interpolate(pos.x))
+            #print(x_min* pixscale, x_max* pixscale, y_min* pixscale, y_max* pixscale, pos.x* pixscale, self.diff_interpolate(pos.x))
             plt.scatter(pos.x[0]* pixscale, pos.x[1]* pixscale, c='g', s=10, marker='x')
             img[i] = (pos.x[0]* pixscale, pos.x[1]*pixscale)
 
-        return img
+        return img              # in arcsec
+
+
+    def get_magnifications(self, h = 1e-9):
+        """
+        Get the magnifications of the images.
+
+        Returns:
+        ---------------
+        magnifications: The magnifications of the images.
+        """
+        def partial_derivative(func, var, point): 
+            args = point[:]
+            def wraps(x):
+                args[var] = x
+                return func(args)
+
+            #print(wraps(point[var]+h), wraps(point[var]-h))
+
+            return lambda x: (wraps(x+h) - wraps(x-h))/(2*h) # central difference diff fct
+
+        def alpha(t):
+            alpha = self.def_angle_interpolate(t[0], t[1])[1]
+            a = float(f"{alpha[0]:.12f}")
+            b = float(f"{alpha[1]:.12f}")
+            return np.array([a, b])
+
+        theta = np.array(self.get_image_positions())/self.pixscale    #in pixel
+        magnification = []
+
+        for theta in enumerate(theta):
+            dalpha1_dtheta1 = partial_derivative(lambda t: alpha(t)[0], 0, theta[1])(theta[1][0])
+            dalpha1_dtheta2 = partial_derivative(lambda t: alpha(t)[0], 1, theta[1])(theta[1][1])
+            dalpha2_dtheta1 = partial_derivative(lambda t: alpha(t)[1], 0, theta[1])(theta[1][0])
+            dalpha2_dtheta2 = partial_derivative(lambda t: alpha(t)[1], 1, theta[1])(theta[1][1])
+            #print(dalpha1_dtheta1, dalpha1_dtheta2, dalpha2_dtheta1, dalpha2_dtheta2)
+
+
+            # Construct the magnification tensor
+            a = np.array([
+                [1 - dalpha1_dtheta1, -dalpha1_dtheta2],
+                [-dalpha2_dtheta1, 1 - dalpha2_dtheta2]
+            ])
+
+            # Calculate magnification
+            magnification.append( 1 / np.linalg.det(a))
+
+        return magnification
+    
+    def get_time_delays(self):
+        """
+        Get the time delays of the images.
+
+        Returns:
+        ---------------
+        time_delays: The time delays of the images in days.
+        """
+        
+        theta = self.get_image_positions()     #in arcsec
+        beta = np.array([self.x_src * self.pixscale, self.y_src * self.pixscale])   #in arcsec
+        data_psi_arcsec = self.lens_potential_map  #in arcsec^2
+
+        def psi_interpolate(x,y, psi = data_psi_arcsec):  #(x,y) is img in arcsec 
+            x = x/self.pixscale
+            y = y/self.pixscale
+            dx = x - floor(x)
+            dy = y - floor(y)
+            top_left = np.array(psi[ceil(y), floor(x)]) #to match (y,x) of alpha grid
+            top_right = np.array(psi[ceil(y), ceil(x)])
+            bottom_left = np.array(psi[floor(y), floor(x)])
+            bottom_right = np.array(psi[floor(y), ceil(x)])
+            top = top_left * (1 - dx) + top_right * dx
+            bottom = bottom_left * (1 - dx) + bottom_right * dx
+            psi = top * dy + bottom *(1 - dy)
+            return psi
+
+        def fermat_potential(theta, beta):
+            return 0.5 * (np.linalg.norm(theta - beta)**2) - psi_interpolate(theta[0], theta[1])
+            
+
+        #for i in range(len(theta)):     #pylint: disable=consider-using-enumerate
+            #print(f"Interpolation Fermat potential at {theta[i]}: {fermat_potential(np.array(theta[i]), beta)}")
+
+        # time delay by diff of fermat potentials and scale it by time-delay distance
+        dt = []
+        for i in range(len(theta)):  #pylint: disable=consider-using-enumerate
+            dt.append(fermat_potential(np.array(theta[i]), beta) - fermat_potential(np.array(theta[0]), beta))
+            #print(f"demensionless time delay at {theta[i]}: {dt[i]}")
+
+        # Redshifts
+        z_L = 0.5
+        z_S = 1.0
+
+        # Calculate distances
+        cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+        D_L = cosmo.angular_diameter_distance(z_L)
+        D_S = cosmo.angular_diameter_distance(z_S)
+        D_LS = cosmo.angular_diameter_distance_z1z2(z_L, z_S)
+        #print(D_LS)
+        time_delay_distance = (1 + z_L) * D_L * D_S / D_LS * const.Mpc
+        #print(f"Time-delay distance: {time_delay_distance.value}")
+        dt_days = np.array(dt) * time_delay_distance.value / const.c / const.day_s * const.arcsec ** 2
+        print(f"Numerical time delay in days: {dt_days} days")
+        return dt_days
+
+
+    
+
+        
